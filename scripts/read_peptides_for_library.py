163975459e7235fa7fde6a22e2bbad21f60d4e6f
jlapin1 <47603871+jlapin1@users.noreply.github.com>
2023-02-11 13:17:29 -0600

    Add files via upload

---
+++ scripts/read_peptides_for_library.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 12:13:37 2023
+
+@author: jsl6
+
+Create labels and comments from tsv files that Qian sends me 
+"""
+
+filepath = "C:/Users/jsl6/Documents/Paper3/library_labels/tsv/uniprotHM20191015Labels/"
+
+fnm_labels = 'uniprotHM20191015_label52.tsv'
+fnm_prot = 'uniprotHM20191015_protlabels.tsv'
+
+with open(filepath + fnm_prot, 'r') as f:
+    # pin: protein index number
+    # name: protein name
+    A = {99999:'Protein=sp|P99999|Multiprotein'}
+    for line in f:
+        [pin, name] = line.split('\t')
+        A[int(pin)] = name.strip()
+
+with open(filepath + fnm_labels, 'r') as f:
+    # In: global index number
+    # pin: protein index number
+    # pm: precursor mass
+    # seq: sequence
+    # charge: precursor charge
+    # mod: modification string
+    # nce: NCE
+    # ev: eV
+    B = {'In':[], 'pin':[], 'pm':[], 'seq':[], 'charge':[], 'mod':[],
+         'nce':[], 'ev':[]}
+    dts = [int, int, float, str, int, str, float, float]
+    for line in f:
+        split = line.strip().split('\t')
+        assert len(split)==len(B.keys())
+        for key,entry,typ in zip(B.keys(), split, dts):
+            B[key].append(typ(entry))
+
+comments = [
+    'Protein=%s'%A[pin] for pin in B['pin']
+]
+labels = [
+    '%s/%d_%s_%.1feV_NCE%.0f'%(
+        B['seq'][m], B['charge'][m], B['mod'][m], B['ev'][m], B['nce'][m]
+    ) for m in range(len(B['seq']))
+]
+
+with open("C:/Users/jsl6/Documents/Paper3/library_labels/uniprot_library/uniprotHM20191015_label52.txt",'w') as f:
+    f.write("\n".join(labels))
+# with open("C:/Users/jsl6/Documents/Paper3/library_labels/uniprot_library/uniprot_comments_40.txt",'w') as f:
+#     f.write("\n".join(comments))
+    
